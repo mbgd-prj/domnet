72624d1bebfd3dc7c9c0aab971796591dc445b4f
Hirokazu Chiba <hchiba1@gmail.com>
2022-02-18 08:12:38 +0900

    Modify lib/plot_powerlaw.py to fit line

--- lib/plot_powerlaw.py
+++ lib/plot_powerlaw.py
@@ -11,6 +11,10 @@ cin = csv.reader(sys.stdin)
 for row in cin:
     nums = [int(n) for n in row]
     results = powerlaw.Fit(nums)
-    fig = powerlaw.plot_pdf(nums, color='tab:blue')
-    powerlaw.plot_ccdf(nums, ax=fig, color='tab:orange')
+    fig = results.plot_pdf(color='tab:orange', label='pdf')
+    results.power_law.plot_pdf(ax=fig, color='tab:orange', linestyle=':')
+    results.plot_ccdf(ax=fig, color='tab:blue', label='ccdf')
+    results.power_law.plot_ccdf(ax=fig, color='tab:blue', linestyle=':')
+    plt.xlim(1,100)
+    plt.legend()
     plt.show()
