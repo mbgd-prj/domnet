9d74f7dcb5b640637d818237ad0939102b2fe344
Hirokazu Chiba <hchiba1@gmail.com>
2022-02-17 09:14:51 +0900

    Add lib/fit_powerlaw.py

---
+++ lib/fit_powerlaw.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import powerlaw
+import sys
+import csv
+
+f = open('test_powerlaw_exponential', mode='w')
+
+cin = csv.reader(sys.stdin)
+for row in cin:
+    nums = [int(n) for n in row]
+    results = powerlaw.Fit(nums)
+
+    # print('a=', results.power_law.alpha)
+    # print('s=', results.power_law.sigma)
+    # print('xmin=', results.power_law.xmin)
+
+    # powerlaw.plot_pdf(nums, color='b')
+
+    # R, p = results.distribution_compare('power_law', 'lognormal')
+    R, p = results.distribution_compare('power_law', 'exponential')
+    # print(R, p)
+    print(R, p, file=f, sep='\t')
+    # f.write(R + p)
+
+f.close()
