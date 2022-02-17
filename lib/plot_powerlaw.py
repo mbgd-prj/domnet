97e741a940dce6013efb293cd60f92e8334c2a86
Hirokazu Chiba <hchiba1@gmail.com>
2022-02-18 08:20:05 +0900

    Modify lib/plot_powerlaw.py to change legends

--- lib/plot_powerlaw.py
+++ lib/plot_powerlaw.py
@@ -11,9 +11,9 @@ cin = csv.reader(sys.stdin)
 for row in cin:
     nums = [int(n) for n in row]
     results = powerlaw.Fit(nums)
-    fig = results.plot_ccdf(color='tab:orange', label='ccdf')
+    fig = results.plot_ccdf(color='tab:orange', label='cumulative distribution function')
     results.power_law.plot_ccdf(ax=fig, color='tab:orange', linestyle=':')
-    results.plot_pdf(ax=fig, color='tab:blue', label='pdf')
+    results.plot_pdf(ax=fig, color='tab:blue', label='probability density function')
     results.power_law.plot_pdf(ax=fig, color='tab:blue', linestyle=':')
     plt.xlim(1,100)
     plt.legend()
