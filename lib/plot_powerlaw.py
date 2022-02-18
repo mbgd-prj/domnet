a5b16ef6b723dd38397f4063dc98486212fb408f
Hirokazu Chiba <hchiba1@gmail.com>
2022-02-18 09:25:16 +0900

    Modify lib/plot_powerlaw.py to output values

--- lib/plot_powerlaw.py
+++ lib/plot_powerlaw.py
@@ -11,6 +11,21 @@ cin = csv.reader(sys.stdin)
 for row in cin:
     nums = [int(n) for n in row]
     results = powerlaw.Fit(nums)
+
+    print('ccdf')
+    x, y = results.ccdf()
+    if len(x) != len(y):
+        sys.exit(1)
+    for i in range(len(x)):
+        print(int(x[i]), y[i], sep='\t')
+
+    print('pdf')
+    bin, prob = results.pdf()
+    if len(bin) != len(prob) + 1:
+        sys.exit(1)
+    for i in range(len(prob)):
+        print(f'{int(bin[i])}-{int(bin[i+1])}', prob[i], sep='\t')
+
     fig = results.plot_ccdf(color='tab:orange', label='cumulative distribution function', linewidth=1, marker='.')
     results.power_law.plot_ccdf(ax=fig, color='tab:orange', linestyle=':', linewidth=1)
     results.plot_pdf(ax=fig, color='tab:blue', label='probability density function', linewidth=1, marker='.')
