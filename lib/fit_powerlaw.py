898cd352261b44d4d79cda9f3af05dd229472a1b
Hirokazu Chiba <hchiba1@gmail.com>
2022-02-18 11:15:08 +0900

    Modify lib/fit_powerlaw.py to output alpha

--- lib/fit_powerlaw.py
+++ lib/fit_powerlaw.py
@@ -16,16 +16,9 @@ cin = csv.reader(sys.stdin)
 for row in cin:
     nums = [int(n) for n in row]
     results = powerlaw.Fit(nums, xmin=1.0)
-
-    # print('a=', results.power_law.alpha)
-    # print('s=', results.power_law.sigma)
-    # print('xmin=', results.power_law.xmin)
-
-    # powerlaw.plot_pdf(nums, color='b')
-
     # R, p = results.distribution_compare('power_law', 'lognormal')
     R, p = results.distribution_compare('power_law', 'exponential')
-    print(R, p, file=f, sep='\t', flush=True)
+    print(R, p, results.alpha, file=f, sep='\t', flush=True)
 
 if args.outfile:
     f.close()
